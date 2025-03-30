3bc3857314d15b12396928be388838085604652f
lgtomio <lgigarachi@gmail.com>
2025-03-30 16:45:55 -0300

    Creating asset folder Inserting all assets Creating the menu Creating te const file

---
+++ code/Const.py
@@ -0,0 +1,14 @@
+# Cores
+C_WHITE=(255,255,255)
+C_RED=(176, 13, 46)
+
+#Menu - Opções
+MENU_OPTION=('NEW GAME 1P',
+             'NEW GAME 2P - COOPERATIVE',
+             'NEW GAME 2P - COMPETITIVE',
+             'SCORE',
+             "EXIT")
+
+#Tamanho da janela
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/game.py
+++ code/game.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 600))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
         while True:
